2d634b7d0e51cfbaaaf6d2466291ccb87c8f5170
wx <584400256@qq.com>
2017-10-24 21:20:13 +0800

    Initial commit

---
+++ src/wx/test/Test.java
@@ -0,0 +1,15 @@
+/**
+ * print hi
+ */
+package wx.test;
+//first code
+
+public class Test {
+    /**
+     *
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
